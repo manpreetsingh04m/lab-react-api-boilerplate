0272160e2a1d3aeeaa574739201de6478682ccb3
manpreetsingh04m <ms2404mk@gmail.com>
2024-01-18 22:56:26 +0530

    Done

---
+++ Api/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Books from './components/Books';
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Books />
+    </div>
+  );
+};
+
+export default App;
